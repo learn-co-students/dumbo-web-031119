36f066fbebba9b33884e50d765ad325ca128332f
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-04-09 11:36:33 -0400

    validations start:

---
+++ 17-rails-validations/README.md
@@ -0,0 +1,6 @@
+# Key Questions
+* What is a validation and what does it have to do with ActiveRecord?
+* When does validation happen?
+* How can I incorporate validation into my controllers?
+* When should I use a custom validator?
+* What validations does ActiveRecord have built-in? How can I find out more?
---
+++ 17-rails-validations/cat-tracker/app/controllers/cats_controller.rb
@@ -0,0 +1,38 @@
+class CatsController < ApplicationController
+  
+  before_action :get_cat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cats = Cat.all
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+    redirect_to @cat
+  end
+
+  def update
+    @cat.update(cat_params)
+    redirect_to @cat
+  end
+
+  def destroy
+    @cat.destroy
+    redirect_to cats_path
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :tail_length, :color_hexadecimal)
+  end
+
+  def get_cat
+    @cat = Cat.find(params[:id])
+  end
+
+end
