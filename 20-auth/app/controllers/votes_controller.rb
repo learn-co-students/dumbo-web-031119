4f344f9f7b955527001675daccaa58b7f68c7d1d
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-04-16 14:03:45 -0400

    auth

--- 20-auth/app/controllers/votes_controller.rb
+++ 20-auth/app/controllers/votes_controller.rb
@@ -1,5 +1,9 @@
 class VotesController < ApplicationController
 
+  def index
+    @votes = Vote.all
+  end
+
   def create
     if @can_vote
       @vote = Vote.create(color_id: params[:color_id])
@@ -9,6 +13,10 @@ class VotesController < ApplicationController
       flash["notification"] = "You don't have any votes left 💁‍♀️"
     end
     redirect_to colors_path
+  end
+
+  def delete
+    Vote.destroy(params[:id])
   end
 
 end
--- 20-auth/config/routes.rb
+++ 20-auth/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :colors, only: [:index] do 
+  resources :colors, only: [:index] do
     resources :votes, only: [:create]
   end
+
+  resources :votes, only: [:index, :destroy]
 
   resources :users, only: [:new, :create]
   resources :login, only: [:new, :create, :destroy]
