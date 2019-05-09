b6f6ddd9a0f42d46d8bf31eb6593398174574ce4
kevinmcalear <kevin.mcalear@gmail.com>
2019-05-09 11:00:30 -0400

    adds lecture

---
+++ 29-this/index.js
@@ -0,0 +1,7 @@
+// the keywork "this"
+// - explain "execution context"
+// - 4 Scenarios of `this`
+//  * Scenario 1: `this` gets AUTOMATICALLY bound to the class/instance
+//  * Scenario 2: Functions
+//  * Scenario 3: Arrow Functions
+//  * Scenario 4: bind/call/apply
