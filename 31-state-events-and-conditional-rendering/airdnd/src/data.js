0944c3142ed4f6c8c9732925aa0e8d47a455cfcd
sbal13 <sbal13@gmail.com>
2019-05-13 15:38:07 -0400

    before state, events

---
+++ 31-state-events-and-conditional-rendering/airdnd/src/data.js
@@ -0,0 +1,62 @@
+const cities = [
+  {
+    name: "Wakanda",
+    image_url: "https://www.eastbaytimes.com/wp-content/uploads/2019/02/OCR-L-DIS-NILES-0224-01-1.jpg?w=620",
+    series: "Marvel",
+  },
+  {
+    name: "Asgard",
+    image_url: "https://www.thewrap.com/wp-content/uploads/2017/04/Asgard-Thor-Raganork.jpg",
+    series: "Marvel",
+  },
+  {
+    name: "Mereen",
+    image_url: "https://i.pinimg.com/originals/2c/88/0e/2c880efb6e3ee55173ca4bcb999ef060.jpg",
+    series: "Game of Thrones",
+  },
+  {
+    name: "King's Landing",
+    image_url: "https://i.redd.it/uul1u9b2h3v01.jpg",
+    series: "Game of Thrones",
+  },
+  {
+    name: "Hogsmeade",
+    image_url: "https://data.whicdn.com/images/323574325/large.jpg",
+    series: "Harry Potter",
+  },
+  {
+    name: "Hogwarts",
+    image_url: "http://cdn4.tillthemoneyrunsout.com/wp-content/uploads/2015/11/Hogwarts-Castle-as-Seen-from-Dragon-Challenge-Line-Visiting-Harry-Potter-World-Orlando1-980x653.jpg"
+    series: "Harry Potter",
+  },
+  {
+    name: "Pallet Town",
+    image_url: "https://vignette.wikia.nocookie.net/pokemon-planet/images/6/6d/NewpalletTown.PNG/revision/latest?cb=20180212155137",
+    series: "Pokemon",
+  },
+  {
+    name: "Cair Paravel",
+    image_url: "https://data.whicdn.com/images/250009424/large.jpg",
+    series: "Chronicles of Narnia",
+  },
+  {
+    name: "Candy Castle",
+    image_url: "https://i.pinimg.com/originals/b6/d3/21/b6d3212638dfd1ea5364f1044565d4e7.jpg",
+    series: "Candyland",
+  },
+  {
+    name: "Minas Tirith",
+    image_url: "https://boygeniusreport.files.wordpress.com/2015/08/minas-tirith-image-001.jpg?quality=98&strip=all&w=782",
+    series: "Lord of the Rings",
+  },
+  {
+    name: "The Shire",
+    image_url: "https://i.pinimg.com/originals/3a/73/5f/3a735fcfb6f4bd6c37b9f10477a449f3.jpg",
+    series: "Lord of the Rings",
+  },
+  {
+    name: "The Mines of Moria",
+    image_url: "https://i.pinimg.com/originals/4f/99/30/4f99300102b2d967efa02ca3008f9f48.jpg",
+    series: "Lord of the Rings",
+  }
+]
